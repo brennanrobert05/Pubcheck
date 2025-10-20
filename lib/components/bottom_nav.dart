aff150a8f7358e4e56b0a38860162aeb0311de88
brennanrobert05 <brennanrobert05@gmail.com>
2025-10-20 23:59:52 +0100

    Began working on Bottom nav

---
+++ lib/components/bottom_nav.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '/pages/homepage.dart';
+import '/pages/nearby_pubs_page.dart';
+import '/pages/profile_page.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = const [Homepage(), NearbyPubs(), ProfilePage()];
+
+  void _tapItem(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black87,
+        selectedItemColor: Colors.amberAccent,
+        unselectedItemColor: Colors.white,
+        currentIndex: _selectedIndex,
+        onTap: _tapItem,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pin_drop_rounded),
+            label: 'Nearby',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+        ]
+      ),
+    )
+  }
+}
